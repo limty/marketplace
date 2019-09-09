ea74e20b024c928ecfa2032cf25d8211426c817e
Yew <limtheanyew@gmail.com>
2019-09-09 15:03:17 +0800

    Done items form

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
 
     validates :title, :brand, :price, :model, presence: true
     validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed. "}
-    validates :title, length : { maximum: 140, too_long: "%{count} characters is the maximum allowed. "}
+    validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed. "}
     validates :price, numericality: {only_integer: true}, length: { maximum: 7 }
 
     CONDITION = %w{ New Excellent Mint Used Fair Poor }
